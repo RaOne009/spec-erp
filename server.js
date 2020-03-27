0304dd7c6d27b205c3a68ad381f792ff4ebc3ced
Rohit Sharma <rs1257933@gmail.com>
2020-03-27 23:06:54 +0530

    bodyParser added

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const connectDB = require("./config/db");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 // Connect Database
 connectDB();
 
@@ -18,8 +21,9 @@ app.use("/api/admin", require("./routes/api/Admin/Admin"));
 app.use("/api/students", require("./routes/api/Users/Student"));
 app.use("/api/teachers", require("./routes/api/Users/Teacher"));
 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`running at http://127.0.0.1:${PORT}/`);
 });
