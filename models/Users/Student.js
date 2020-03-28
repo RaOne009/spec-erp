6d06154a04c9195c500ea5763be805547c2cc8a3
Rohit Sharma <rs1257933@gmail.com>
2020-03-28 22:11:24 +0530

    ready for heroku

--- models/Users/Student.js
+++ models/Users/Student.js
@@ -1,5 +1,46 @@
-const mongoose = require("mongoose");
+var mongoose = require("mongoose");
 
-const StudentSchema = mongoose.Schema({});
+var studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    class: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Class",
+        required: true
+    },
+    fatherName: {
+        type: String,
+        required: true
+    },
+    motherName: {
+        type: String,
+        required: true
+    },
+    address: String,
+    dob: {
+        type: Date,
+        required: true
+    },
+    aadharNumber: String,
+    accountNumber: String,
+    SSSMID: String,
+    photo: String,
+    enrollmentNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    rollNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        minlength: 6
+    }
+});
 
-module.exports = Student = mongoose.model("Student", StudentSchema);
+module.exports = mongoose.model("student", studentSchema);
